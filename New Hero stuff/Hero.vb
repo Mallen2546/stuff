2972c55233b5a1e18faa246b45e1d81999959b3b
ab41987 <mallen2546@gmail.com>
2018-09-21 16:14:54 -0500

    update

--- New Hero stuff/Hero.vb
+++ New Hero stuff/Hero.vb
@@ -1,6 +1,66 @@
 ﻿Public Class Hero
     Private _Name As String
     Private _Quote As String
+    Private _Move1 As String
+    Private _Move2 As String
+    Private _Move3 As String
+    Private _Move4 As String
+    Private _Miss As String
+    Private _PowerMove As String
 
-    Public New (ByVal Name As String, Quote As String)
+
+    Sub New(ByVal Name As String, Quote As String, M1 As String, M2 As String, M3 As String, M4 As String, Power As String, Miss As String)
+        _Name = Name
+        _Quote = Quote
+        _Move1 = M1
+        _Move2 = M2
+        _Move3 = M3
+        _Move4 = M4
+        _PowerMove = Power
+        _Miss = Miss
+
+
+    End Sub
+
+    Public ReadOnly Property Name As String
+        Get
+            Return _Name
+        End Get
+    End Property
+    Public ReadOnly Property Quote As String
+        Get
+            Return _Quote
+        End Get
+    End Property
+    Public ReadOnly Property M1 As String
+        Get
+            Return _Move1
+        End Get
+    End Property
+    Public ReadOnly Property M2 As String
+        Get
+            Return _Move2
+        End Get
+    End Property
+    Public ReadOnly Property M3 As String
+        Get
+            Return _Move3
+        End Get
+    End Property
+    Public ReadOnly Property M4 As String
+        Get
+            Return _Move4
+        End Get
+    End Property
+    Public ReadOnly Property Power As String
+        Get
+            Return _PowerMove
+        End Get
+    End Property
+    Public ReadOnly Property Miss As String
+        Get
+            Return _Miss
+        End Get
+    End Property
+
 End Class
--- New Hero stuff/HeroArray.vb
+++ New Hero stuff/HeroArray.vb
@@ -1,14 +1,19 @@
 ﻿Module HeroArray
     Dim HeroQuotes As ArrayList = New ArrayList()
+    Dim Thor As Hero = New Hero("Thor", "Touch my hammer.", "Lightning Bolt", "Hammer Throw", "Thunder Punch", "Hammer Storm", "Storm Breaker", "Miss")
+    Dim Hulk As Hero = New Hero("Hulk", "HULK SMASH!", "Smash", "Smash Wave", "Ground Smash", "Double Smash", "Raging Smash", "Miss")
+    Dim CaptainAmerica = New Hero("CaptainAmerican", "Language!", "Shield Bash", "Shield Slam", "Shield Toss", "Patriot Punch", "Star Spangled Slammer", "Miss")
+    Dim Hawkeye As Hero = New Hero("Hawkeye", "Nobody would know.", "Trap Shot", "Full draw", "Triple Shot", "Quick shot", "Nuclear Arrow", "Miss")
+
     Sub Quote()
 
         Dim HQ = HeroQuotes
 
         Dim Thor = "Touch my hammer."
-        Dim Hulk = "Omaha smash."
-        Dim CaptainAmerica = "Should loan out my shield."
+        Dim Hulk = "HULK SMASH!"
+        Dim CaptainAmerica = "Language!"
         Dim BlackWidow = "I have special skills acquired over a long and lustrious "
-        Dim Hawkeye = "I am a hero too."
+        Dim Hawkeye = "Nobody would know."
         Dim IronMan = "Cap and I are very close."
         HQ.Add(Thor)
         HQ.Add(Hulk)
@@ -17,4 +22,47 @@
         HQ.Add(Hawkeye)
         HQ.Add(IronMan)
     End Sub
+
+    ReadOnly HeroMoves As ArrayList = New ArrayList()
+    Sub Moves()
+        Randomize()
+        Dim generate As New Random
+        Dim move = generate.Next(0, 5)
+        HeroMoves.Add(Thor.M1)
+        HeroMoves.Add(Thor.M2)
+        HeroMoves.Add(Thor.M3)
+        HeroMoves.Add(Thor.M4)
+        HeroMoves.Add(Thor.Power)
+        HeroMoves.Add(Thor.Miss)
+
+        Dim result = HeroMoves.Item(move)
+
+        Select Case result
+            Case Thor.M1
+
+
+        End Select
+
+    End Sub
+
+
+    Sub Moves(Hulk)
+        Randomize()
+        Dim generate As New Random
+        Dim move = generate.Next(0, 5)
+        HeroMoves.Add(Hulk)
+        HeroMoves.Add(Hulk)
+        HeroMoves.Add(Hulk)
+        HeroMoves.Add(Hulk)
+        HeroMoves.Add(Hulk)
+        HeroMoves.Add(Hulk)
+
+
+
+
+    End Sub
+
+
+
+
 End Module
